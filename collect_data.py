6d41d0b1f6273bc4e52f1a15d7af643f229d2837
Trần Quang Thành <95174252+thanhthanhhp123@users.noreply.github.com>
2023-03-02 17:40:12 +0700

    update

--- collect_data.py
+++ collect_data.py
@@ -6,8 +6,8 @@ start = 0
 name = 'thanh'
 i = 0
 cap = cv2.VideoCapture(1)
-# cap.set(1280)
-# cap.set(640)
+cap.set(3, 1280)
+cap.set(4, 640)
 try:
     os.mkdir('Datasets/'+name)
 except:
@@ -15,12 +15,13 @@ except:
 while True:
     ret, frame = cap.read()
     #sleep 500ms
+    i+=1
     now = time.time()
     if(now - start >= 500):
-        cv2.imwrite('Datasets/'+name+f'/image{i+1}.jpg', frame)
+        cv2.imwrite('Datasets/'+name+f'/image{i}.jpg', frame)
         start = now
     cv2.imshow('Capture', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):   
         break
     
 cap.release()
--- load_data.py
+++ load_data.py
@@ -2,15 +2,25 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from algorithms import LogisticRegression
 
-datas_folders = os.listdir('Datasets')
-data_folders = []
-for i in datas_folders:
-    a = os.path.join('Datasets/', i)
-    data_folders.append(a)
+# datas_folders = os.listdir('Datasets')
+# data_folders = []
+# for i in datas_folders:
+#     a = os.path.join('Datasets/', i)
+#     data_folders.append(a)
 
 # print(data_folders)
 
-for i in data_folders:
-    a = os.listdir(i)
-    print(np.array(a).shape)
+hung_folder = 'Datasets/Test/'
+hung_folder_ = [os.path.join(hung_folder, i) for i in os.listdir(hung_folder)]
+hung_img = []
+for i in hung_folder_:
+    hung_img.append(cv2.imread(i))
+
+fig, ax = plt.subplots(1, len(hung_img))
+for i in range(len(hung_img)):
+    ax[i].imshow(hung_img[i], cmap = 'gray')
+    ax[i].axis('off')
+
+# print(np.array(hung_img))
